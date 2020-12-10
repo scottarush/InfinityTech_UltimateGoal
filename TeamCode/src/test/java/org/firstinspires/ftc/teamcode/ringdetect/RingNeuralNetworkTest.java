8fcd72ea11921dea3b220c32c168dac41bda8e52
Scott Rush <scottarush@yahoo.com>
2020-12-09 21:38:34 -0500

    Initial debugged version of neural network

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/ringdetect/RingNeuralNetworkTest.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/ringdetect/RingNeuralNetworkTest.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.ringdetect;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,12 +20,30 @@ public class RingNeuralNetworkTest {
         // Now create and pass training data to neural network
         JavaNeuralNetwork ringnn = new JavaNeuralNetwork(new int[]{13,15,3});
 
-
-//        ringnn.train(processor.getXTrainingData(),processor.getYTrainingData());
-
+        final StringBuffer logBuffer = new StringBuffer();
+        logBuffer.append("Epoch#,Normal Error\n");
+        ringnn.addTrainingStatusListener(new JavaNeuralNetwork.ITrainingStatusListener() {
+            @Override
+            public void trainingStatus(int epochNumber, double normalError) {
+                logBuffer.append(epochNumber+","+ normalError +"\n");
+            }
+        });
+        ringnn.train(processor.getXTrainingData(),processor.getYTrainingData(),5,0.005d,50000);
+        try{
+            File logFile = new File(filePath+"/training_log.csv");
+            if (logFile.exists()){
+                logFile.delete();
+            }
+            FileWriter fw = new FileWriter(logFile);
+            fw.write(logBuffer.toString());
+            fw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
 
         // print out the weights
-       System.out.print(ringnn.printNetwork());
+//        System.out.print(ringnn.printNetwork());
 
     }
 }
