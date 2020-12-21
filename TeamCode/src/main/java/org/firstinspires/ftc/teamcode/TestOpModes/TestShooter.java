747e85ba88c70806da0d0898790618aa349fc1f2
kylewr <kylewrush@gmail.com>
2020-12-21 16:44:18 -0500

    Complete but not tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/TestShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/TestShooter.java
@@ -1,17 +1,25 @@
 package org.firstinspires.ftc.teamcode.TestOpModes;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.shooter.Shooter;
 
 @TeleOp(name="TestShooter", group="robot")
+//@Disabled
 public class TestShooter extends OpMode {
     private Shooter mShooter = null;
-
+    // Make sure the left motor is called "shooterL" in the configuration and "shooterR" for the right motor
+    // the servo needs to be called "pusherServo", and can be disabled. see init() for more info
     @Override
     public void init() {
         mShooter = new Shooter();
+        // to disable the servos, uncomment (take out "//") this next line:
+
+        //mShooter.disableServo();
+
+        // check for initialization errors
         try {
             mShooter.init(hardwareMap);
         } catch (Exception e) {
@@ -23,16 +31,25 @@ public class TestShooter extends OpMode {
     @Override
     public void loop() {
         // listen for gamepad input and do the corresponding function
+        // x button starts the shooter
         boolean gpx = gamepad1.x;
+        // a button shoots a ring
         boolean gpa = gamepad1.a;
+        // b button deactivates the shooter
         boolean gpb = gamepad1.b;
+        // y button actuates the servo
+        boolean gpy = gamepad1.y;
         if (gpx) {
+            // start shooter and print telemetry
             mShooter.activateShooter();
+            telemetry.addData("status:", "ready to shoot!");
+            telemetry.update();
         }
         if (gpa) {
             boolean shootStatus = mShooter.shoot();
+            // check if the shooter is ready to shoot
             if (shootStatus == false) {
-                telemetry.addData("status:", "shooter not ready");
+                telemetry.addData("status:", "shooter not ready!");
                 telemetry.update();
             } else if (shootStatus) {
                 telemetry.addData("status:", "shot the ring :)");
@@ -40,9 +57,20 @@ public class TestShooter extends OpMode {
             }
         }
         if (gpb) {
+            // brake the motors
             mShooter.deactivateShooter();
             telemetry.addData("status:", "shooter deactivated");
             telemetry.update();
+        }
+        if (gpy) {
+            boolean servoEnabled = mShooter.checkServoStatus();
+            if (servoEnabled) {
+                mShooter.actuateServo();
+            } else {
+                // print error if not
+                telemetry.addData("status:", "Servo not enabled! Cannot run the function!");
+                telemetry.update();
+            }
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/Shooter.java
@@ -5,12 +5,25 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Shooter{
+    // left motor is to the left when looking directly at the robot from the front and vice versa for right
     private DcMotor mLeftMotor = null;
     private DcMotor mRightMotor = null;
     private Servo mPusherServo = null;
+    private boolean servoActuated = false;
+    public boolean servoEnabled = true;
+    public static final int PUSHER_RETRACTED = 0;
+    public static final int PUSHER_EXTENDED = 1;
 
-    public Shooter(){
+    public Shooter() {}
 
+    public void disableServo() { servoEnabled = false; }
+
+    public boolean checkServoStatus() {
+        if (servoEnabled) {
+            return true;
+        } else {
+            return false;
+        }
     }
     // init the devices
     public void init(HardwareMap ahwMap) throws Exception {
@@ -29,36 +42,75 @@ public class Shooter{
         } catch (Exception e) {
             initErrString += ", Right Shooter Motor error";
         }
-        try {
-            mPusherServo = ahwMap.get(Servo.class, "pusherServo");
-        } catch (Exception e) {
-            initErrString += ", Pusher servo error";
+        if (servoEnabled) {
+            try {
+                mPusherServo = ahwMap.get(Servo.class, "pusherServo");
+                mPusherServo.setPosition(1.0d);
+            } catch (Exception e) {
+                initErrString += ", Pusher servo error";
+            }
+        } else {
+            initErrString += "WARNING: PusherServo not enabled!";
         }
         if (initErrString.length() > 0){
             throw new Exception(initErrString);
         }
     }
     // TODO: add an led on the robot
-
+    boolean readyToShoot = false;
     public void activateShooter() {
-
+        if (readyToShoot) return;
+        readyToShoot = true;
+        mLeftMotor.setPower(1.0d);
+        mRightMotor.setPower(1.0d);
+        if (servoEnabled) {
+            servoActuated = false;
+            setPusher(PUSHER_RETRACTED);
+        }
     }
     public void deactivateShooter() {
-
+        if (readyToShoot == false) return;
+        readyToShoot = false;
+        mLeftMotor.setPower(0d);
+        mLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mRightMotor.setPower(0d);
+        mRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        if (servoEnabled) {
+            servoActuated = false;
+            setPusher(PUSHER_RETRACTED);
+        }
     }
 
     /**
      * Shoots a ring if the shooter is ready.
      * @return True if the shooter shot a ring. False if the shooter isn't ready to shoot.
      */
-    boolean shot = false;
     public boolean shoot() {
-        if (shot) {
-            shot = false;
-            return true;
-        } else {
-            shot = true;
-            return false;
+        if (readyToShoot == false) return false;
+        readyToShoot = false;
+        if (servoEnabled) {
+            setPusher(PUSHER_EXTENDED);
         }
+        return true;
+    }
+    public void actuateServo() {
+        if (servoActuated) {
+            setPusher(PUSHER_RETRACTED);
+        } else {
+            servoActuated = true;
+            setPusher(PUSHER_EXTENDED);
+        }
+    }
+    public void setPusher(int state) {
+        float position = 0;
+        switch (state) {
+            case PUSHER_RETRACTED:
+                position = 1.0f;
+                break;
+            case PUSHER_EXTENDED:
+                position = -1.0f;
+                break;
+        }
+        mPusherServo.setPosition(position);
     }
 }
