2947f39c6d15fb9ea46654f17fbc9df6b71ed16d
Scott Rush <scottarush@yahoo.com>
2020-12-26 16:03:21 -0500

    Fixed bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/gen_autosm_graph.bat
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/gen_autosm_graph.bat
@@ -1,2 +1,2 @@
-java -jar ../../../../../../../../lib/Smc.jar -graph -glevel 2 FilterDevStateMachine.sm
-"C:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tpng FilterDevStateMachine_sm.dot > FilterDevStateMachine_sm.png
+java -jar ../../../../../../../../lib/Smc.jar -graph -glevel 2 ShooterStateMachine.sm
+"C:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tpng ShooterStateMachine_sm.dot > ShooterStateMachine_sm.png
